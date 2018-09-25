d115cd795d9d13ea33346c27016365f95fb2cff1
Sean Martin <sean.martin@gmail.com>
2018-09-25 00:21:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+return true if num == 2
+return false if num <= 1
+  prime = true
+
+  (2..num).each do |n|
+    if num % n == 0
+      prime = false
+    end
+    break if n >= num / 2
+  end
+  prime
+end
+
+p prime?(13)
+p prime?(24)
+p prime?(111)
